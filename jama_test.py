827b1ae83ca263a2031bbe27760e3ff03c6ebbf3
Raja, Chander <craja@idirect.net>
2020-02-04 00:03:29 -0500

    deleted commented code

--- jama_test.py
+++ jama_test.py
@@ -3,8 +3,6 @@ from py_jama_rest_client.client import JamaClient
 import login_dialog
 import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import numpy as np
 
 
 
@@ -146,7 +144,7 @@ def main():
         exit(1)
 
     status_counts = testrun_df['status'].value_counts()
-    colormap = {'NOT_RUN': 'gray', 'PASSED': 'green', 'FAILED': 'red', 'BLOCKED': 'blue', 'INPROGRESS': 'yellow'}
+    colormap = {'NOT_RUN': 'gray', 'PASSED': 'green', 'FAILED': 'red', 'BLOCKED': 'blue', 'INPROGRESS': 'orange'}
 
     pie_colors = []
     for index in status_counts.index:
@@ -161,111 +159,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-'''
-
-# create a dctionary of data frames for all test cycles
-df_cycles = {}
-df_overall = None
-for x in testcycles:
-    testcycle_id = x['id']
-    testcycle_name = x['fields']['name']
-    testruns = jama_client.get_testruns(test_cycle_id=testcycle_id)
-    data_list = list()
-    for y in testruns:
-        data_list.append([testcycle_name,
-                    y['fields']['name'],
-                    y['modifiedDate'],
-                    y['fields']['testRunStatus']])
-    # create a data frame from the test runs for this test cycle
-    df = pd.DataFrame(data_list, columns=['cycle', 'name', 'date', 'status'])
-    # convert 'date' column dtype to datetime
-    df['date'] = pd.to_datetime(df['date'])
-
-    if df_overall is None:
-        df_overall = df
-    else:
-        df_overall = pd.concat([df_overall, df])
-    df_cycles[testcycle_name] = df
-
-#df_cycles['All'] = df_overall
-
-status_counts = df_overall['status'].value_counts()
-status_counts = status_counts.rename('Overall')
-# Build chart data
-x_axis = ['All test cycles']
-for x in testcycles:
-    x_axis.append(x['fields']['name'])
-
-chart_df = pd.DataFrame(columns=['cycle', 'NOT_RUN', 'PASSED', 'FAILED', 'BLOCKED', 'INPROGRESS'])
-chart_df = chart_df.set_index(['cycle'])
-chart_df = chart_df.append(status_counts)
-
-for cycle in df_cycles.keys():
-    status_counts = df_cycles[cycle]['status'].value_counts()
-    status_counts = status_counts.rename(cycle)
-    chart_df = chart_df.append((status_counts))
-
-colors_dict = { 'NOT_RUN': 'gray', 'PASSED': 'green', 'FAILED': 'red', 'BLOCKED': 'blue', 'INPROGRESS': 'yellow'}
-'''
-'''
-chart_data = []
-for status, column in chart_df.iteritems():
-    chart_data.append(go.Bar(name=status, x=column.index.tolist(), y=column.values.tolist(), marker_color=colors_dict[status]))
-
-fig = go.Figure(data=chart_data)
-# Change the bar mode
-fig.update_layout(barmode='stack', title_text='PIT FAT2 Dry Run Status')
-fig.show()
-
-'''
-
-'''
-# Pie chart subplots
-num_traces = chart_df.shape[0]
-num_rows = 3
-num_cols = int(num_traces/3) +1
-specs = [
-    [{'type':'domain'},]*num_cols,
-] * num_rows
-
-fig = make_subplots(
-    rows=num_rows, cols=num_cols,
-    specs=specs,
-    subplot_titles=list(chart_df.index.values)
-)
-fig.print_grid()
-next_row = 1
-next_col = 1
-for test_cycle, row in chart_df.iterrows():
-    fig.add_trace(go.Pie(name=test_cycle, labels=row.index, values=row.values, textinfo='label+percent',
-                             insidetextorientation='radial', scalegroup='one'),
-                  row=next_row,
-                  col=next_col
-                  )
-    next_col += 1
-    if next_col > num_cols:
-        next_col = 1
-        next_row += 1
-
-
-fig.update_layout(height=800, width=600, title_text='PIT FAT2 Dry Run Status')
-fig.show()
-
-'''
-'''
-pie_row = chart_df.loc['Overall']
-pie_colors = []
-for index in pie_row.index:
-    pie_colors.append(colors_dict[index])
-fig = go.Figure(data=[go.Pie(labels=pie_row.index, values=pie_row.values,
-                             textinfo='label+percent',
-                             insidetextorientation='radial',
-                             marker_colors=pie_colors
-                             )])
-fig.update_layout(title_text='PIT FAT2 Dry Run Status')
-fig.show()
-'''
