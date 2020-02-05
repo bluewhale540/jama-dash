8bdd9774249bc0ca55c7be058284ebdb6c933253
Chander Raja <chander.raja@gmail.com>
2020-02-05 17:37:34 -0500

    added menu to select which test cycle to display a chart of

--- main.py
+++ main.py
@@ -20,28 +20,76 @@ def display_current_status_chart(df_status, status_names, colormap, title):
     fig.update_layout(title_text=title)
     fig.show()
 
-def display_historical_status_chart(df_status, status_names, colormap, title, deadline=None):
-    # create historical status scatter graph
-    fig = go.Figure()
+def display_historical_status_chart(df_list, status_names, colormap, title_list, deadline=None):
+    if df_list is None or len(df_list) == 0:
+        print('empty data set. Aborting line chart')
+        return
 
-    x_axis = [pd.to_datetime(d).date() for d in df_status['date'].values]
+    if len(df_list) != len(title_list):
+        print('inconsistency between data and title lists. Aboting line chart')
+        return
+
+    # create historical status scatter graph
+    x_list = [] # list of x axis data
+    y_list = [] # list of dict of y-axis with status name as key
+    deadline_x_list = []
+    deadline_y_list = []
+    for df in df_list:
+        x_list.append([pd.to_datetime(d).date() for d in df['date'].values])
+        y_dict = {}
+        for status in status_names:
+            y_dict[status] = df[status].values
+        y_list.append(y_dict)
+        if deadline is not None:
+            tail = df.tail(1)
+            current_date = pd.to_datetime(tail['date'].values[0])
+            deadline_x = [current_date, pd.to_datetime(deadline)]
+            deadline_y = [tail['NOT_RUN'].values[0], 0]
+            deadline_x_list.append(deadline_x)
+            deadline_y_list.append(deadline_y)
+
+    df = df_list[0]
+    x_axis = x_list[0]
+    title = title_list[0]
     # Add traces
+    data = []
     for status in status_names:
-        y_axis = df_status[status].values
-        fig.add_trace(go.Scatter(x=x_axis, y=y_axis,
+        y_axis = y_list[0][status]
+        data.append(go.Scatter(x=x_axis, y=y_axis,
                                  mode='lines',
                                  name=status,
                                  line=dict(color=colormap[status])
                                  ))
     # add deadline meeting trace
-    tail = df_status.tail(1)
-    current_date = pd.to_datetime(tail['date'].values[0])
-    deadline_x = [current_date, pd.to_datetime(deadline)]
-    deadline_y = [tail['NOT_RUN'].values[0], 0]
-    fig.add_trace(go.Scatter(x=deadline_x, y=deadline_y,
-                             mode='lines',
-                             line=dict(dash='dash', color='black')))
-    fig.update_layout(title_text=title)
+    if deadline is not None:
+        data.append(go.Scatter(x=deadline_x_list[0], y=deadline_y_list[0],
+                                 mode='lines', name='required burn rate',
+                                 line=dict(dash='dash', color='black')))
+
+    updatemenus= []
+    menu = {}
+    menu['buttons'] = []
+    for i in range(0, len(x_list)):
+        button = {}
+        button['method'] = 'restyle'
+        button['label'] = title_list[i]
+        button['args'] = []
+        arg = {}
+        arg['x'] = []
+        arg['y'] = []
+        for status in status_names:
+            arg['x'].append(x_list[i])
+            arg['y'].append(y_list[i][status])
+        arg['x'].append(deadline_x_list[i])
+        arg['y'].append(deadline_y_list[i])
+        arg['title'] = title_list[i]
+        button['args'].append(arg)
+        menu['buttons'].append(button)
+    menu['direction'] = 'down'
+    menu['showactive'] = True
+    updatemenus.append(menu)
+    layout = go.Layout(updatemenus=updatemenus, title=title_list[0])
+    fig = go.Figure(data=data, layout=layout)
     fig.show()
 
 def main():
@@ -80,27 +128,33 @@ def main():
         if testcycle_db is None:
             exit(1)
 
-        testcycles = [None]
+        testcycles = [None,]
         for id, cycle in testcycle_db:
             testcycles.append(cycle)
-
+        title_list = []
+        df_list = []
         for cycle in testcycles:
-            chart_title = title
-            if cycle is not None:
-                chart_title += ' (' + cycle + ')'
-            df_status_by_date = client.get_testrun_status_historical(project_key=project,
+            if cycle is None:
+                title_list.append(title)
+            else:
+                title_list.append(cycle)
+
+            df = client.get_testrun_status_historical(project_key=project,
                                                                      testplan_key=testplan,
                                                                      testcycle_key=cycle)
-            if df_status_by_date is None:
+            if df is None:
                 continue
-            local_tz = get_localzone()
-            deadline_date = datetime.strptime('2020-02-28-05:00:00', '%Y-%m-%d-%H:%M:%S').replace(tzinfo=pytz.utc).astimezone(local_tz)
-            display_historical_status_chart(df_status=df_status_by_date,
-                                            status_names=status_names,
-                                            colormap=colormap,
-                                            title=chart_title,
-                                            deadline=deadline_date)
+            df_list.append(df)
 
+        local_tz = get_localzone()
+        deadline_date = datetime.strptime('2020-02-28-05:00:00', '%Y-%m-%d-%H:%M:%S').replace(tzinfo=pytz.utc).astimezone(local_tz)
+        display_historical_status_chart(df_list=df_list,
+                                        status_names=status_names,
+                                        colormap=colormap,
+                                        title_list=title_list,
+                                        deadline=deadline_date)
+
+'''
         for cycle in testcycles:
             df_status_current = client.get_testrun_status_current(project_key=project,
                                                                   testplan_key=testplan,
@@ -109,6 +163,7 @@ def main():
                 continue
             display_current_status_chart(df_status=df_status_current,
                                          status_names=status_names, colormap=colormap, title=chart_title)
+'''
 
 if __name__ == '__main__':
     main()
